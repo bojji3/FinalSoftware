26eab86c7905b0c6fe083ca40ca524e3a02e7bc4
bojji3 <kvisaya@gbox.adnu.edu.ph>
2024-05-27 18:51:37 +0800

    edited the driver's controllers

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,15 @@
 class Reservation < ApplicationRecord
   belongs_to :passenger
   belongs_to :trip
+
+  after_create :update_trip_stats
+
+  private
+
+  def update_trip_stats
+    trip.update(
+      number_of_passengers: trip.reservations.count,
+      total_fare: trip.reservations.count * trip.unit_fare
+    )
+  end
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,2 +1,5 @@
 class Trip < ApplicationRecord
+  belongs_to :bus
+  has_many :reservations
+  has_many :passengers, through: :reservations
 end
